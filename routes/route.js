b080ef189538898552faff3c4a4fe392c2b9c236
donaldteghen <donaldteghen@gmail.com>
2021-01-10 20:20:55 +0300

    Made some changes in the route.js file to reflect update request bu adding an endpoint for put.

--- routes/route.js
+++ routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { findByIdAndUpdate } = require('./models/contact');
 const router = express.Router();
 
 const Contact = require('./models/contact');
@@ -51,14 +52,38 @@ router.post('/contact', async(req, res, next)=>{
     })
 })
 
-
+// update with put
+router.put('/contacts/:id',  async(req, res, next)=>{
+    var _id = req.params.id;
+    if(!_id){
+        res.status(500).json({"Error Message": "No matching document found!", "id": _id});
+    }
+    else{
+        //res.send(_id);
+        var contact = new Contact({
+            firstName: req.body.firstName,
+            lastName:req.body.lastName,
+            phoneNumber:req.body.phoneNumber
+        });
+        await Contact.updateOne({_id:req.params.id},{firstName:req.body.firstName, lastName:req.body.lastName, phoneNumber:req.body.phoneNumber},
+            [ upsert=false], (err, doc)=>{
+            if(err) {
+                res.status(500).json({"Error Message": "Something went wrong!"});
+            }
+            else{
+                res.json(doc);
+            }
+        })
+    }
+    
+});
 
 
 router.delete('/contact/:id', async(req, res, next)=>{
     var id = req.params.id;
     await Contact.deleteOne({_id:id}, (err, contact) =>{
         if(err) {
-            res.status(500).json({"Error Message": "Something went wrong!"});
+            res.status(500).json({"Error Message": "Something went wrong!"}); 
         }
         else{
             res.json(contact);
